e20a0d945031683446fcc833a6edc5566386a1b7
SkywingsWang <skywing316@outlook.com>
2023-05-31 01:45:40 +0100

    Update smaller_image.R

--- smaller_image.R
+++ smaller_image.R
@@ -2,18 +2,21 @@ install.packages("magick")
 
 library(magick)
 
-# Edit path 
-# 编辑文件路径
+# Set Path
+# 设置导入和导出路径
 image_path <- "something.jpg"
+output_path <- "something.jpg"
 
 image <- image_read(image_path)
 
-# Set the target file size (here set to 1M or less) 
-# 设置目标文件大小（这里设置1M以内）
+# Set the target file size (here set to 1M or less)
+# 设置目标文件大小（1M以内）
 target_size <- 5e5
 
-while (image_file_size(image) > target_size) {
-  image <- image_scale(image, "50%")  
-}
+original_size <- file.info(image_path)$size
 
-image_write(image, path = image_path)
+scale <- sqrt(target_size / original_size)
+
+compressed_image <- image_resize(image, paste0(as.integer(scale * 100), "%"))
+
+image_write(compressed_image, path = output_path)
